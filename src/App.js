8cf4fa90120b6e2ca726a902052f63de9f8b523f
sz-sebestyen <szabosebestyen.mail@gmail.com>
2021-02-01 23:40:29 +0100

    Add form component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import MMBoard from "./components/MMBoard/MMBoard.component";
+import ContextForm from "./components/ContextForm/ContextForm";
 
 import "./App.css";
 
 function App() {
   return (
     <div className="App" onDragStart={(event) => event.preventDefault()}>
-      <MMBoard />
+      {/* <MMBoard /> */}
+      <ContextForm />
     </div>
   );
 }
---
+++ src/components/ContextForm/ContextForm.jsx
@@ -0,0 +1,41 @@
+import style from "./ContextForm.module.css";
+
+const ContextForm = (props) => {
+  return (
+    <form className={style.contextForm}>
+      <div className={style.firstName}>
+        <label htmlFor="contextFormFirstName">First name</label>
+        <input
+          type="text"
+          name="contextFormFirstName"
+          id="contextFormFirstName"
+        />
+      </div>
+      <div className={style.lastName}>
+        <label htmlFor="contextFormLastName">Last name</label>
+        <input
+          type="text"
+          name="contextFormLastName"
+          id="contextFormLastName"
+        />
+      </div>
+      <div className={style.company}>
+        <label htmlFor="contextFormCompany">Company</label>
+        <input type="text" name="contextFormCompany" id="contextFormCompany" />
+      </div>
+      <div className={style.position}>
+        <label htmlFor="contextFormPosition">Position</label>
+        <input
+          type="text"
+          name="contextFormPosition"
+          id="contextFormPosition"
+        />
+      </div>
+      <div className={style.submit}>
+        <button type="button">Save</button>
+      </div>
+    </form>
+  );
+};
+
+export default ContextForm;
