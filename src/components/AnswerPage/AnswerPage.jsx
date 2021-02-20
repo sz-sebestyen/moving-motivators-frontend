61f814e1fa2d065e6d9d42a500ba5896a3827b31
sz-sebestyen <szabosebestyen.mail@gmail.com>
2021-02-20 17:46:52 +0100

    Add answerpage button states

--- src/components/AnswerPage/AnswerPage.jsx
+++ src/components/AnswerPage/AnswerPage.jsx
@@ -20,60 +20,98 @@ const AnswerPage = (props) => {
   const { /* groupId, */ questionId } = useParams();
   const [userContext, setUserContext] = useContext(UserContext);
   const [questionsContext, setQuestionsContext] = useContext(QuestionsContext);
+
   const [question, setQuestion] = useState(
     questionsContext.find((que) => que.id.toString() === questionId)
   );
+  useEffect(() => {
+    setQuestion(
+      questionsContext.find((que) => que.id.toString() === questionId)
+    );
+  }, [questionsContext]);
+
   const note = useRef(null);
   const [saveCards, setSaveCards] = useState();
 
   const defaultCards = useCards(userContext.user.defaultCardListId);
   const previousCards = useCards(question && question.answerId);
 
+  const [saveStatus, setSaveStatus] = useState();
+  const [finalizeStatus, setFinalizeStatus] = useState();
+
   useEffect(() => console.log("question: ", question));
 
   const Save = async () => {
     if (!question) return;
+
+    setSaveStatus("loading");
+
+    let setAnsAnswer;
+    let noteAnswer;
+
     if (saveCards) {
       console.log("cards to be saved:", saveCards);
-      const setAnsAnswer = await setAnswer(question.id, saveCards);
+      setAnsAnswer = await setAnswer(question.id, saveCards);
       console.log("setAnswer answer:", setAnsAnswer);
     }
 
     if (note.current.value !== "") {
-      const noteAnswer = await editNote(question.id, note.current.value);
+      noteAnswer = await editNote(question.id, note.current.value);
       console.log("noteAnswer:", noteAnswer);
     }
-    setUserContext((prev) => ({ ...prev, dataLoaded: false }));
+
+    if (setAnsAnswer || noteAnswer) {
+      setSaveStatus("done");
+      setUserContext((prev) => ({ ...prev, dataLoaded: false }));
+    } else {
+      setSaveStatus();
+    }
   };
+
+  useEffect(() => {
+    setSaveStatus();
+  }, [saveCards]);
 
   /**
    * Finalize the answer to the question. It will no longer be editable.
    */
   const Close = async () => {
     if (!question) return;
+    setFinalizeStatus("loading");
     const closeAnswre = await closeQuestion(question.id);
     console.log("closeAnswer:", closeAnswre);
+    setFinalizeStatus();
+
+    // TODO: set question closed
     setUserContext((prev) => ({ ...prev, dataLoaded: false }));
   };
 
   return (
     <main className="answerPage">
       <div className="answerMenu">
-        <ButtonConfirm
-          type="button"
-          onClick={Save}
-          disabled={question ? question.closed : question}
-        >
-          Save
-        </ButtonConfirm>
+        {question && question.closed ? (
+          <ButtonDecline disabled>Finalized</ButtonDecline>
+        ) : (
+          <>
+            <ButtonConfirm
+              type="button"
+              onClick={Save}
+              disabled={saveStatus || finalizeStatus}
+              state={saveStatus}
+            >
+              Save
+            </ButtonConfirm>
 
-        <ButtonDecline
-          type="button"
-          onClick={Close}
-          disabled={question ? question.closed : question}
-        >
-          Finalize
-        </ButtonDecline>
+            <ButtonDecline
+              type="button"
+              onClick={Close}
+              disabled={finalizeStatus || finalizeStatus}
+              state={finalizeStatus}
+            >
+              Finalize
+            </ButtonDecline>
+          </>
+        )}
       </div>
 
       <h1 className="title">{question ? question.value : ""}</h1>
@@ -88,7 +126,7 @@ const AnswerPage = (props) => {
           ref={note}
           defaultValue={question && question.note}
           placeholder="save a note"
-          disabled={question ? question.closed : question}
+          disabled={question && question.closed}
           style={{ resize: "none" }}
         ></textarea>
       </div>
